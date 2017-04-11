895ed7d35dd572a8102baedbd91b84ddf40c7e66
Federico Mirás <miras.federico@gmail.com>
2017-04-11 05:27:06 -0300

    Format about component code

--- components/about.js
+++ components/about.js
@@ -8,18 +8,30 @@ export default () => (
               <img src="/static/images/fmiras.jpg" alt=""/>
             </div>
           </div>
-          <div className="col m12 s12 co-centered wow fadeIn a2" data-wow-delay="0.2s">
+          <div
+            className="col m12 s12 co-centered wow fadeIn a2"
+            data-wow-delay="0.2s"
+            >
             <div className="intro-content">
               <h2>Federico Mirás</h2>
               <span>Hacker & Entrepeneur</span>
-              <p>Hi! My name is Federico, you can just call me &quot;fefo&quot;.
-              I&apos;m a 20-years old software developer and entrepreneurship enthusiastic
-              always searching, discussing or creating ideas to turn them into high-tech projects.
-              I&apos;m also a freelance developer, If you are thinking on working with me I&apos;ll be glad
-              to hear your idea, I just recommend you to read some of my <a href="#blog"><span>words</span></a>, and see some of my <a href="https://github.com/fmiras"><span>code</span></a> (even this <a href="https://github.com/fmiras/site"><span>site</span></a> is available to see!).
+              <p>
+                Hi! My name is Federico, you can just call me &quot;fefo&quot;.
+                I&apos;m a 20-years old software developer and entrepreneurship
+                enthusiastic always searching, discussing or creating ideas to
+                turn them into high-tech projects. I&apos;m also a freelance
+                developer, If you are thinking on working with me I&apos;ll be
+                glad to hear your idea, I just recommend you to read some of
+                my <a href="#blog"><span>words</span></a>, and see some of
+                my <a href="https://github.com/fmiras"><span>code</span></a>
+                (even this
+                <a href="https://github.com/fmiras/site"><span>site</span></a>
+                is available to see!).
               </p>
               <a href="#" className="btn waves-effect">Download CV</a>
-              <a href="#contact-form" className="btn btn-success waves-effect">Contact Me</a>
+              <a href="#contact-form" className="btn btn-success waves-effect">
+                Contact Me
+              </a>
             </div>
           </div>
         </div>
